18b198dab0afe67e6fbf435e72d927544dfcb5e6
Kabsa_Abdi <KabsaAbdi23@gmail.com>
2019-09-18 12:41:24 -0700

    linked css

--- app.py
+++ app.py
@@ -16,16 +16,17 @@ def index():
     # b) your API key, 'key'
     # c) how many GIFs to return, 'limit'
     params = {'q': query,
-        'key': 'DT7VKCNIRM18',
+        'key': 'F9X97G66FSHO',
         'limit': '8'}
 
     r = requests.get("https://api.tenor.com/v1/search", params=params)
+     # TODO: Use the '.json()' function to get the JSON of the returned response
     gifJson = r.json()
+    gifDisplay = gifJson['results'] # Getting results attribute from the json object
+
     title =  'Search for Gifs!'
-    gifDisplay = gifJson['results']
-
-
     return render_template("index.html",title = title,query = query,gifDisplay = gifDisplay)
+    # Two lines above are showing the gifs and title and rendering the index file
 
 if __name__ == '__main__':
     app.run(debug=True)
